fc6e60bfccc38abd294ce11631afb8fef3652968
vasilevisovan <vasile.visovan@yahoo.com>
2023-12-06 10:12:56 -0800

    #5

--- Class 2 Exercises and Homework.js
+++ Class 2 Exercises and Homework.js
@@ -28,17 +28,18 @@ console.log('My favourite food is ', vasileVisovan.favouriteFood);
 // -XO
 // X-X
 const ticTacToe = [
-  ['-','o','-'],
-  ['-','x','o'],
-  ['x','-','x']
+  ['-','O','-'],
+  ['-','X','-'],
+  ['X','-','X']
 ];
-// 4. After the array is created, 'O' claims the top right square.
-// Update that value.
-const newLineTicTacToe = [['-','-','o']];
-const slicedTicTacToe = ticTacToe.slice(-2);
 
-// 5. Log the grid to the console.
-newLineTicTacToe.concat(slicedTicTacToe);
+// O claims the top right square (0-based indexing)
+ticTacToe[0][2] = 'O';
+
+// Log the updated grid to the console
+for (let row = 0; row < 3; row++) {
+  console.log(ticTacToe[row].join(' '));
+}
 
 // 6. You are given an email as string myEmail, make sure it is in correct email format.
 // Should be 1 or more characters, then @ sign, then 1 or more characters, then dot, then one or more characters - no whitespace
@@ -59,55 +60,38 @@ isValidEmail(email2);//false
 
 // 7. You are given an assignmentDate as a string in the format "month/day/year"
 // i.e. '1/21/2019' - but this could be any date.
-function stringToDate(dateString) {
-  
-  const date = new Date(dateString);
-   
-  if (isNaN(date)) {
-    return null; // Return null if the conversion failed
-  }
-
-  return date;
-}
-// Convert this string to a Date
-const assignmentDate = '1/21/2019';
-stringToDate(assignmentDate);
-
 
 // 8. Create a new Date instance to represent the dueDate.  
 // This will be exactly 7 days after the assignment date.
-
 
 // 9. Use dueDate values to create an HTML time tag in format
 // <time datetime="YYYY-MM-DD">Month day, year</time>
 // I have provided a months array to help
 
+// Given assignmentDate string
+const assignmentDateStr = '1/21/2019';
 
-// Split the date string and extract the month, day, and year
-// Example assignment date in the 'M/D/YYYY' format
+// Convert the assignmentDate string to a Date object
+const assignmentDate = new Date(assignmentDateStr);
 
-
-// Manually extract the month, day, and year
-const dateComponents = assignmentDate.split('/');
-const month = parseInt(dateComponents[0], 10);
-const day = parseInt(dateComponents[1], 10);
-const year = parseInt(dateComponents[2], 10);
-
-// Create the assignmentDate
-const assignmentDate = new Date(year, month - 1, day); // Subtract 1 from month (0-based)
-
-// Calculate the dueDate as 10 days after the assignment date
+// Create a new Date instance for the dueDate (7 days after assignment date)
 const dueDate = new Date(assignmentDate);
-dueDate.setDate(assignmentDate.getDate() + 7);
+dueDate.setDate(dueDate.getDate() + 7);
 
-// Define the months array
+// Months array to help with formatting
 const months = [
   'January', 'February', 'March', 'April', 'May', 'June',
   'July', 'August', 'September', 'October', 'November', 'December'
 ];
 
-// Format the dueDate as an HTML time tag
-const formattedDueDate = `<time datetime="${dueDate.toISOString().slice(0, 10)}">${months[dueDate.getMonth()]} ${dueDate.getDate()}, ${dueDate.getFullYear()}</time>`;
+// Format the dueDate for HTML time tag
+const formattedDueDate = `${dueDate.getFullYear()}-${(dueDate.getMonth() + 1).toString().padStart(2, '0')}-${dueDate.getDate().toString().padStart(2, '0')}`;
+const monthName = months[dueDate.getMonth()];
+const day = dueDate.getDate();
+const year = dueDate.getFullYear();
 
-// 10. log this value using console.log
-console.log("Formatted Due Date:", formattedDueDate);
+// Create HTML time tag
+const htmlTimeTag = `<time datetime="${formattedDueDate}">${monthName} ${day}, ${year}</time>`;
+
+// Log the result to the console
+console.log(htmlTimeTag);
